b1a14a4af78773e5d771fa88515c91d865d47a11
corpglory-dev <dev@corpglory.com>
2020-01-24 20:35:34 +0300

    Init annotations

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,11 @@
+// TODO: it's not the best timeseries type
 export type TimeSeries = {
   columns: string[],
   values: number[][],
   colors: string[]
 };
+
+export type Annotation = {
+  timestamp: number,
+  text: string
+}
