e0e882a809bb95305435436a3e8fa1c0d76a1e11
corpglory-dev <dev@corpglory.com>
2020-02-04 14:21:12 +0300

    onClick -> click event && decouple timeseries prop

--- src/types.ts
+++ src/types.ts
@@ -1,10 +1,3 @@
-// TODO: it's not the best timeseries type
-export type TimeSeries = {
-  columns: string[],
-  values: number[][],
-  colors: string[]
-};
-
 export type Annotation = {
   timestamp: number,
   text: string
