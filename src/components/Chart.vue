e4aa284fbcf88babcc4344205d063e4a77498eec
corpglory-dev <dev@corpglory.com>
2020-02-07 16:09:38 +0300

    fix

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -329,7 +329,7 @@ export default class MyChart extends Vue {
 
     this.$emit('mouse-move', {
       point: [x, y],
-      mouse: [d3.event.clientX - 125, d3.event.clientY - 60],
+      mouse: [d3.event.clientX - 125, d3.event.clientY + 30],
       value: `${d[1].toFixed(2)}`
     });
 
